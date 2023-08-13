c321b8fed014cd2d58c22ff723ff6b057e063775
thienNhan <thnhan032@gmail.com>
2023-08-13 12:06:32 +0700

    Test git

--- WhatsFood/app/src/main/java/com/example/whatsfood/Activity/MainActivity.java
+++ WhatsFood/app/src/main/java/com/example/whatsfood/Activity/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+import com.example.whatsfood.Activity.Admin.AdminBottomNavigationActivity;
 import com.example.whatsfood.Activity.Buyer.BuyerBottomNavigationActivity;
+import com.example.whatsfood.Activity.Buyer.RegisterBuyerActivity;
 import com.example.whatsfood.Activity.Seller.SellerBottomNavigationActivity;
 import com.example.whatsfood.Activity.Seller.SellerOrderDetailsActivity;
 import com.example.whatsfood.Activity.Seller.SellerViewSelectedFoodActivity;
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 //                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                startActivity(new Intent(MainActivity.this, SellerBottomNavigationActivity.class));
+                startActivity(new Intent(MainActivity.this, AdminBottomNavigationActivity.class));
                 finish();
             }
         }, 900);
